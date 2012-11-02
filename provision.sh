b1de2ec4900736e2e1b1bd2a52609f2a0d1709b3
Justin <justin@x230.(none)>
2012-11-01 22:07:47 -0400

    provision nfsen

--- provision.sh
+++ provision.sh
@@ -7,10 +7,25 @@ apt-get upgrade -y
 
 apt-get install -y nfdump
 
-apt-get install -y libapache2-mod-php5   librrds-perl libmailtools-perl rrdtool whois
+apt-get install -y libapache2-mod-php5   librrds-perl libmailtools-perl libsocket6-perl rrdtool whois
 
 if [ ! -e /etc/apache2/mods-enabled/rewrite.load ] ; then
     a2enmod rewrite
+    service apache2 restart
 fi
 
-service apache2 restart
+
+if [ ! -e nfsen-1.3.6p1.tar.gz ] ; then
+    wget http://iweb.dl.sourceforge.net/project/nfsen/stable/nfsen-1.3.6p1/nfsen-1.3.6p1.tar.gz
+    tar xvzf nfsen-1.3.6p1.tar.gz
+fi
+
+if [ ! -e /var/www/nfsen ] ; then 
+    cd nfsen-1.3.6p1/
+    yes ''| ./install.pl /vagrant_data/nfsen.conf
+    cd /var/www/nfsen
+    ln -s nfsen.php index.php
+fi
+
+/data/nfsen/bin/nfsen stop
+/data/nfsen/bin/nfsen start
